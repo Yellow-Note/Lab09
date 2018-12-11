37ed3274de952907f36bcd1ffb3e6ac203392cc8
Yellow-Note <38773929+Yellow-Note@users.noreply.github.com>
2018-12-10 20:59:38 -0700

    Lab 9 files

---
+++ main.cpp
@@ -0,0 +1,73 @@
+// Lab9.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+
+map<string, int> FileRead(map<string, int> stringMap) {//map<key,value>
+	string fileName, word;
+	map<string, int>::iterator it;
+	cout << "Enter name of file to read from" << endl;
+	cin >> fileName;
+	ifstream inFile;
+	inFile.open(fileName);
+	if (inFile.is_open()) {
+		while (inFile >> word) {
+			it = stringMap.find(word);
+			if (it != stringMap.end()) {//find element ++
+				++stringMap[word];
+			}
+			else {//not find element insert into map
+				stringMap.insert(pair<string, int>(word, 1));
+			}
+			word.clear();
+		}
+	}
+	else {
+		cout << "Failed to open file" << endl;
+	}
+
+	inFile.close();
+	inFile.clear();
+
+	return stringMap;
+}
+
+void FileWrite(map<string, int> stringMap) {
+	string fileName;
+	cout << "Enter name of file to write from" << endl;
+	cin >> fileName;
+	ofstream outFile;
+	outFile.open(fileName);
+	if (outFile.is_open()) {
+		for (map<string, int>::iterator it = stringMap.begin(); it != stringMap.end(); ++it) {
+			while (it->second != 0) {
+				outFile << it->first << " ";
+				it->second--;
+			}
+			outFile << endl;
+		}
+	}
+	else {
+		cout << "Failed to open file to write" << endl;
+	}
+
+	outFile.close();
+	outFile.clear();
+}
+
+int main()
+{
+	map<string, int> stringMap;
+
+	stringMap = FileRead(stringMap);
+	FileWrite(stringMap);
+
+    return 0;
+}
